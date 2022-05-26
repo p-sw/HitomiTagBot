4cc8605a54026709e20351ab244dddc7f8b19b97
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-05-26 12:43:22 +0000

    Moved database settings from product setting to base setting

--- settings/base.py
+++ settings/base.py
@@ -1,3 +1,23 @@
 import os
 
+class InvalidEnvironVariableError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return "INVALID ENV_VAR: "+self.msg
+
 bot_token = os.environ.get('BOTTOKEN')
+
+if bot_token is None:
+    raise InvalidEnvironVariableError('BOTTOKEN')
+
+DATABASE = {
+    "DATABASE_HOST": os.environ.get('DATABASE_HOST'),
+    "DATABASE": os.environ.get('DATABASE'),
+    "DATABASE_USER": os.environ.get('DATABASE_USER'),
+    "DATABASE_PORT": "5432",
+    "DATABASE_PASSWORD": os.environ.get('DATABASE_PASSWORD')
+}
+
+if None in DATABASE.values():
+    raise InvalidEnvironVariableError(list(DATABASE.keys)[list(DATABASE.values).index(None)])
