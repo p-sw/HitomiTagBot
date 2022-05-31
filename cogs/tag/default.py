00e54f450cca311fef3658c157e1c89dca4ee5fa
sserve-kr <61446372+sserve-kr@users.noreply.github.com>
2022-05-31 12:29:19 +0000

    Added tester command

--- cogs/tag/default.py
+++ cogs/tag/default.py
@@ -1,12 +1,19 @@
+import discord
 from discord.ext import commands
+from settings import tester_ids
 
 class MainCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
-    async def search(ctx):
-        await ctx.respond("Search")
+    @commands.slash_command(guild_ids=tester_ids)
+    async def test_sum(
+        self,
+        ctx, 
+        first: discord.Option(int), 
+        second: discord.Option(int)
+    ):
+        await ctx.respond(str(first+second))
 
 def setup(bot):
     bot.add_cog(MainCommand(bot))
